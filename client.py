7567ba4f7031eed64909b07e2a09babc97798e6f
Matt Banister <matthew.banister@gmail.com>
2014-02-04 13:26:26 -0500

    Add kwargs parameter to Diffbot.request that can be used to pass optional parameters to API

--- client.py
+++ client.py
@@ -5,13 +5,15 @@ class DiffbotClient(object):
     def __init__(self):
         self.base_url = "http://api.diffbot.com/"
 
-    def request(self, url, token, api, fields=[], version=2):
+    def request(self, url, token, api, fields=[], version=2, **kwargs):
         """
         Returns a python object containing the requested resource from the diffbot api
         """
         params = {"url": url, "token": token}
         if fields:
             params['fields'] = fields
+        if kwargs:
+            params.update(kwargs)
         response = requests.get(self.compose_url(api, version), params=params)
         obj = response.json()
         return obj
