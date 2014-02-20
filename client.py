9b8ab8b6604de158b28ae52b6ce9bebc4bafe388
Attila Oláh <attilaolah@gmail.com>
2014-02-20 13:10:48 +0700

    rm temporary variable

--- client.py
+++ client.py
@@ -15,8 +15,7 @@ class DiffbotClient(object):
         if kwargs:
             params.update(kwargs)
         response = requests.get(self.compose_url(api, version), params=params)
-        obj = response.json()
-        return obj
+        return response.json()
 
     def compose_url(self, api, version_number):
         """
