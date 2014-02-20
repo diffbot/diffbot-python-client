ce4001734728ad787d61a7833e9242192ab507ba
Attila Oláh <attilaolah@gmail.com>
2014-02-20 13:13:15 +0700

    use string formatting

--- client.py
+++ client.py
@@ -22,13 +22,11 @@ class DiffbotClient(object):
         Returns the uri for an endpoint as a string
         """
         version = self.format_version_string(version_number)
-        url = self.base_url + version + "/" + api
-        return url
+        return '{}{}/{}'.format(self.base_url, version, api)
 
     @staticmethod
     def format_version_string(version_number):
         """
         Returns a string representation of the API version
         """
-        version = "v" + str(version_number)
-        return version
+        return '{}v'.format(version_number)
