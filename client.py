88d375a4028a1c3f7e6c3fb125f0dd50c28ccf3e
Attila Oláh <attilaolah@gmail.com>
2014-02-20 13:23:09 +0700

    remove redundant emptiness check of kwargs before update

--- client.py
+++ client.py
@@ -12,8 +12,7 @@ class DiffbotClient(object):
         params = {"url": url, "token": token}
         if fields:
             params['fields'] = fields
-        if kwargs:
-            params.update(kwargs)
+        params.update(kwargs)
         response = requests.get(self.compose_url(api, version), params=params)
         response.raise_for_status()
         return response.json()
